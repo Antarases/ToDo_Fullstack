d6937306d29c711ec1794894bc35c5a97fafc2d8
Yaroslav Grushchak <Antarases@gmail.com>
2019-12-03 21:26:56 +0200

    IXiaXWyB: Fixing issue with WebSocket connection closing.

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -4,6 +4,6 @@ module.exports = function(app) {
     app.use(proxy("/auth/google", { target: "http://localhost:5000/" }));
     app.use(proxy("/api/*", { target: "http://localhost:5000/" }));
 
-    app.use(proxy("/todos_ws", { target: "http://localhost:5000/" }));
-    app.use(proxy("/chats_ws", { target: "http://localhost:5000/" }));
+    app.use(proxy("/todos_ws", { target: "http://localhost:5000/", ws: true }));
+    app.use(proxy("/chats_ws", { target: "http://localhost:5000/", ws: true }));
 };
